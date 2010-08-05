1148a0cde6d0521e8440354d8f07e686b3342dc0
Kisoo <ksoo.yu@samsung.com>
2010-08-05 18:34:33 +0900

    [general] declair registers in clock, serial header file

--- arch/arm/plat-samsung/include/plat/regs-serial.h
+++ arch/arm/plat-samsung/include/plat/regs-serial.h
@@ -53,6 +53,9 @@
 #define S3C2410_UERSTAT	  (0x14)
 #define S3C2410_UFSTAT	  (0x18)
 #define S3C2410_UMSTAT	  (0x1C)
+#define S3C2410_UBRDIV	  (0x28)
+#define S3C2410_UDIVSLOT  (0x2C)
+#define S3C2410_UINTMSK   (0x38)
 
 #define S3C2410_LCON_CFGMASK	  ((0xF<<3)|(0x3))
 
@@ -194,6 +197,11 @@
 #define S3C64XX_UINTSP		0x34
 #define S3C64XX_UINTM		0x38
 
+/* S5V210 interrupt registers. */
+#define S5P_UINTP		0x30
+#define S5P_UINTSP		0x34
+#define S5P_UINTM		0x38
+
 /* Following are specific to S5PV210 and S5P6442 */
 #define S5PV210_UCON_CLKMASK	(1<<10)
 #define S5PV210_UCON_PCLK	(0<<10)
@@ -257,7 +265,11 @@ struct s3c2410_uartcfg {
 	unsigned char	   hwport;	 /* hardware port number */
 	unsigned char	   unused;
 	unsigned short	   flags;
+#if !defined(CONFIG_CPU_S5PV210)
 	upf_t		   uart_flags;	 /* default uart flags */
+#else
+        unsigned long	   uart_flags;      /* default uart flags */
+#endif
 
 	unsigned long	   ucon;	 /* value of ucon for port */
 	unsigned long	   ulcon;	 /* value of ulcon for port */
