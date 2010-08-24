bed2433f1e9a50d8ee3d6282c3bed4f068545719
huisung.kang <hs1218.kang@samsung.com>
2010-08-25 04:35:10 +0900

    S5PC110: PM: Add 'arch_reset()' to support reboot function.
    This patch add 'arch_reset()' to support reboot function using watchdog.
    
    Change-Id: I949dc88f977324c4215a55feeca357a35b259e62

--- arch/arm/mach-s5pv210/include/mach/system.h
+++ arch/arm/mach-s5pv210/include/mach/system.h
@@ -13,6 +13,10 @@
 #ifndef __ASM_ARCH_SYSTEM_H
 #define __ASM_ARCH_SYSTEM_H __FILE__
 
+#include <mach/map.h>
+#include <linux/clk.h>
+#include <linux/io.h>
+
 static void arch_idle(void)
 {
 	/* nothing here yet */
@@ -20,6 +24,32 @@ static void arch_idle(void)
 
 static void arch_reset(char mode, const char *cmd)
 {
+	int ret;
+	void __iomem *wdt;
+	static struct clk *wdt_clock;
+
+	wdt_clock = clk_get(NULL, "watchdog");
+	if (IS_ERR(wdt_clock)) {
+		printk(KERN_ERR "failed to find watch dog clock\n");
+		ret = PTR_ERR(wdt_clock);
+		return ;
+	}
+	clk_enable(wdt_clock);
+
+	wdt = ioremap(S5PV210_PA_WATCHDOG, 0x400);
+	if (wdt == NULL) {
+		printk(KERN_ERR "failed to ioremap regin in arch_reset\n");
+		return;
+	}
+
+	__raw_writel(0x0, wdt); /* Disable WDT */
+	__raw_writel(0x8000, wdt + 0x4);
+	__raw_writel(0x1, wdt + 4);
+	__raw_writel(0x8000, wdt + 8);
+	__raw_writel(0x8, wdt + 8);
+	__raw_writel(0x8021, wdt);
+
+	mdelay(500);
 	/* nothing here yet */
 }
 
