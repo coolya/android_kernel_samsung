d09d0e6fea0b31dd16ae2cd53c1143dc8f79cbde
huisung.kang <hs1218.kang@samsung.com>
2010-09-21 03:37:16 -0400

    S5PC11X: FIMC: Fix compile warning.
    Change-Id: I6983d95675404a314e4eeafc12e040589eeda0f8
    Signed-off-by: huisung.kang <hs1218.kang@samsung.com>

--- drivers/media/video/samsung/fimc/csis.c
+++ drivers/media/video/samsung/fimc/csis.c
@@ -268,7 +268,6 @@ static int s3c_csis_clk_on(struct platform_device *pdev)
 {
 	struct s3c_platform_csis *pdata;
 	struct clk *parent, *mout_csis;
-	int ret;
 
 	pdata = to_csis_plat(&pdev->dev);
 
@@ -301,7 +300,6 @@ static int s3c_csis_clk_on(struct platform_device *pdev)
 static int s3c_csis_clk_off(struct platform_device *pdev)
 {
 	struct s3c_platform_csis *plat;
-	int ret;
 
 	plat = to_csis_plat(&pdev->dev);
 
