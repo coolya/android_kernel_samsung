41a60de53f229fd0bb25adeb29faf58bccce58ae
Brian Swetland <swetland@google.com>
2010-10-07 21:40:56 -0700

    S5PC11X: PM: remove per-saved-register debug chatter on resume/restore
    Signed-off-by: Brian Swetland <swetland@google.com>

--- arch/arm/plat-samsung/pm.c
+++ arch/arm/plat-samsung/pm.c
@@ -187,12 +187,8 @@ void s3c_pm_do_save(struct sleep_save *ptr, int count)
 
 void s3c_pm_do_restore(struct sleep_save *ptr, int count)
 {
-	for (; count > 0; count--, ptr++) {
-		printk(KERN_DEBUG "restore %p (restore %08lx, was %08x)\n",
-		       ptr->reg, ptr->val, __raw_readl(ptr->reg));
-
+	for (; count > 0; count--, ptr++)
 		__raw_writel(ptr->val, ptr->reg);
-	}
 }
 
 /**
