17628fdbd83df6b3202d16e6634631d65ca32de0
w12.lee <w12.lee@samsung.com>
2010-10-26 22:44:08 +0900

    S5PC11X: WLAN: Modified wlan gpio for starting/stopping driver in suspend.
    When the driver receives DRIVER STOP command from platform,
    current consumption in suspend mode rises little bit high(+1.5mA) due to floating pins.
    
    So, we will deal with WLAN_BT_EN (REG_ON pin) at wlan_power_en() for routine of resetting bcm4329 module
    and change sleep config of WLAN_nRST pin.
    
    Change-Id: I0e1da1ff0acf4a11b61d931268bdd80efbdfcb63
    Signed-off-by: w12.lee <w12.lee@samsung.com>

--- arch/arm/mach-s5pv210/mach-herring.c
+++ arch/arm/mach-s5pv210/mach-herring.c
@@ -3783,11 +3783,30 @@ static int wlan_power_en(int onoff)
 		s3c_gpio_setpull(GPIO_WLAN_nRST, S3C_GPIO_PULL_NONE);
 		gpio_set_value(GPIO_WLAN_nRST, GPIO_LEVEL_HIGH);
 		s3c_gpio_slp_cfgpin(GPIO_WLAN_nRST, S3C_GPIO_SLP_OUT1);
+		s3c_gpio_slp_setpull_updown(GPIO_WLAN_nRST, S3C_GPIO_PULL_NONE);
+
+		s3c_gpio_cfgpin(GPIO_WLAN_BT_EN, S3C_GPIO_OUTPUT);
+		s3c_gpio_setpull(GPIO_WLAN_BT_EN, S3C_GPIO_PULL_NONE);
+		gpio_set_value(GPIO_WLAN_BT_EN, GPIO_LEVEL_HIGH);
+		s3c_gpio_slp_cfgpin(GPIO_WLAN_BT_EN, S3C_GPIO_SLP_OUT1);
+		s3c_gpio_slp_setpull_updown(GPIO_WLAN_BT_EN,
+					S3C_GPIO_PULL_NONE);
 
 		msleep(80);
 	} else {
 		gpio_set_value(GPIO_WLAN_nRST, GPIO_LEVEL_LOW);
 		s3c_gpio_slp_cfgpin(GPIO_WLAN_nRST, S3C_GPIO_SLP_OUT0);
+		s3c_gpio_slp_setpull_updown(GPIO_WLAN_nRST, S3C_GPIO_PULL_NONE);
+
+		if (gpio_get_value(GPIO_BT_nRST) == 0) {
+			gpio_set_value(GPIO_WLAN_BT_EN, GPIO_LEVEL_LOW);
+			s3c_gpio_slp_cfgpin(GPIO_WLAN_BT_EN, S3C_GPIO_SLP_OUT0);
+			s3c_gpio_slp_cfgpin(GPIO_WLAN_nRST, S3C_GPIO_SLP_OUT1);
+			s3c_gpio_slp_setpull_updown(GPIO_WLAN_nRST,
+						S3C_GPIO_PULL_NONE);
+			s3c_gpio_slp_setpull_updown(GPIO_WLAN_BT_EN,
+						S3C_GPIO_PULL_NONE);
+		}
 	}
 	return 0;
 }
@@ -3805,11 +3824,6 @@ static int wlan_carddetect_en(int onoff)
 	u32 sdio;
 
 	if (onoff) {
-		s3c_gpio_cfgpin(GPIO_WLAN_BT_EN, S3C_GPIO_OUTPUT);
-		s3c_gpio_setpull(GPIO_WLAN_BT_EN, S3C_GPIO_PULL_NONE);
-		gpio_set_value(GPIO_WLAN_BT_EN, GPIO_LEVEL_HIGH);
-		s3c_gpio_slp_cfgpin(GPIO_WLAN_BT_EN, S3C_GPIO_SLP_OUT1);
-
 		for (i = 0; i < ARRAY_SIZE(wlan_sdio_on_table); i++) {
 			sdio = wlan_sdio_on_table[i][0];
 			s3c_gpio_cfgpin(sdio,
@@ -3819,11 +3833,6 @@ static int wlan_carddetect_en(int onoff)
 				gpio_set_value(sdio, wlan_sdio_on_table[i][2]);
 		}
 	} else {
-		if (gpio_get_value(GPIO_BT_nRST) == 0) {
-			gpio_set_value(GPIO_WLAN_BT_EN, GPIO_LEVEL_LOW);
-			s3c_gpio_slp_cfgpin(GPIO_WLAN_BT_EN, S3C_GPIO_SLP_OUT0);
-		}
-
 		for (i = 0; i < ARRAY_SIZE(wlan_sdio_off_table); i++) {
 			sdio = wlan_sdio_off_table[i][0];
 			s3c_gpio_cfgpin(sdio,
