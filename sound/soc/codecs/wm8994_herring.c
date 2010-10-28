d48dcfec62ceef4851afcf001d210fdc86e371d0
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-10-27 19:03:48 -0700

    ASoC: Increase Herring earpiece mic gain by 6dB to +15dB
    As per Samsung recommendation to meet T-Mobile test standards.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm8994_herring.c
+++ sound/soc/codecs/wm8994_herring.c
@@ -263,7 +263,7 @@ struct gain_info_t voicecall_gain_table[VOICECALL_GAIN_NUM] = {
 		.mode = VOICECALL_RCV,
 		.reg  = WM8994_LEFT_LINE_INPUT_1_2_VOLUME,	/* 18h */
 		.mask = WM8994_IN1L_VOL_MASK,
-		.gain = WM8994_IN1L_VU | 0x11   /* +9dB */
+		.gain = WM8994_IN1L_VU | 0x15   /* +15dB */
 	}, {
 		.mode = VOICECALL_RCV,
 		.reg  = WM8994_INPUT_MIXER_3,		/* 29h */
