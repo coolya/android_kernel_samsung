6cec7522be727e21f5c2565b544b275dc1073b89
Rahul Bansal <rbansal@nvidia.com>
2010-12-13 16:51:46 +0530

    [bcm4329] WOW support in BCM driver
    Making sure that Rx pkt is acknowledged and sent
    to network interface by wifi driver before system
    goes in suspend.
    
    BUG ID 767438
    
    Change-Id: Ie88176ec11aa1d2fe4253d9b0472d13a4742d300
    Reviewed-on: http://git-master/r/12703
    Reviewed-by: Rakesh Kumar <krakesh@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/net/wireless/bcm4329/dhd.h
+++ drivers/net/wireless/bcm4329/dhd.h
@@ -48,6 +48,10 @@
 #include <asm/uaccess.h>
 #include <asm/unaligned.h>
 
+#ifdef CONFIG_HAS_WAKELOCK
+#include <linux/wakelock.h>
+#endif
+
 /* The kernel threading is sdio-specific */
 #else /* LINUX */
 #define ENOMEM		1
@@ -168,6 +172,9 @@ typedef struct dhd_pub {
 	uint8 country_code[WLC_CNTRY_BUF_SZ];
 	char eventmask[WL_EVENTING_MASK_LEN];
 
+#ifdef CONFIG_HAS_WAKELOCK
+	struct wake_lock 	wow_wakelock;
+#endif
 } dhd_pub_t;
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27)) && defined(CONFIG_PM_SLEEP)
