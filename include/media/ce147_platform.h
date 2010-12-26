666e41bf7d96eebe81266a4a64d3dd6c1023fa54
Kolja Dummann <k.dummann@gmail.com>
2010-12-26 11:23:11 +0100

    Add Samsung camera driver for NEC CE147 camera module

---
+++ include/media/ce147_platform.h
@@ -0,0 +1,20 @@
+/*
+ * Driver for CE147 (5MP camera) from NEC
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+struct ce147_platform_data {
+	unsigned int default_width;
+	unsigned int default_height;
+	unsigned int pixelformat;
+	int freq;	/* MCLK in Hz */
+
+	/* This SoC supports Parallel & CSI-2 */
+	int is_mipi;
+	int (*cam_power)(int onoff);
+};
+
