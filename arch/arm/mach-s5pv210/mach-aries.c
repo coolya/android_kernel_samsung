9395883f2a3104976a1077e7f7df2db26b1b95c6
Daniel Hillenbrand <info@codeworkx.de>
2011-01-07 13:56:33 -0800

    mach aries: use wlan_power_en() from herring, cause the aries solution did not work on every phone

--- arch/arm/mach-s5pv210/mach-aries.c
+++ arch/arm/mach-s5pv210/mach-aries.c
@@ -3808,41 +3808,41 @@ void s3c_config_gpio_alive_table(int array_size, int (*gpio_table)[4])
 static int wlan_power_en(int onoff)
 {
 	if (onoff) {
-		s3c_config_gpio_alive_table(ARRAY_SIZE(wlan_gpio_table), wlan_gpio_table);
-		s3c_config_gpio_alive_table(ARRAY_SIZE(wlan_sdio_on_table), wlan_sdio_on_table);
+		s3c_gpio_cfgpin(GPIO_WLAN_HOST_WAKE,
+				S3C_GPIO_SFN(GPIO_WLAN_HOST_WAKE_AF));
+		s3c_gpio_setpull(GPIO_WLAN_HOST_WAKE, S3C_GPIO_PULL_DOWN);
 
-		/* PROTECT this check under spinlock.. No other thread should be touching
-		 * GPIO_BT_REG_ON at this time.. If BT is operational, don't touch it. */
-		spin_lock_irqsave(&wlan_reglock, wlan_reglock_flags);
-		/* need delay between v_bat & reg_on for 2 cycle @ 38.4MHz */
-		udelay(5);
+		s3c_gpio_cfgpin(GPIO_WLAN_WAKE,
+				S3C_GPIO_SFN(GPIO_WLAN_WAKE_AF));
+		s3c_gpio_setpull(GPIO_WLAN_WAKE, S3C_GPIO_PULL_NONE);
+		gpio_set_value(GPIO_WLAN_WAKE, GPIO_LEVEL_LOW);
 
-		gpio_set_value(GPIO_WLAN_BT_EN, GPIO_LEVEL_HIGH);
-		s3c_gpio_slp_cfgpin(GPIO_WLAN_BT_EN, S3C_GPIO_SLP_OUT1);
-
+		s3c_gpio_cfgpin(GPIO_WLAN_nRST,
+				S3C_GPIO_SFN(GPIO_WLAN_nRST_AF));
+		s3c_gpio_setpull(GPIO_WLAN_nRST, S3C_GPIO_PULL_NONE);
 		gpio_set_value(GPIO_WLAN_nRST, GPIO_LEVEL_HIGH);
 		s3c_gpio_slp_cfgpin(GPIO_WLAN_nRST, S3C_GPIO_SLP_OUT1);
+		s3c_gpio_slp_setpull_updown(GPIO_WLAN_nRST, S3C_GPIO_PULL_NONE);
 
-		spin_unlock_irqrestore(&wlan_reglock, wlan_reglock_flags);
-	}
-	else {
-		/* PROTECT this check under spinlock.. No other thread should be touching
-		 * GPIO_BT_REG_ON at this time.. If BT is operational, don't touch it. */
-		spin_lock_irqsave(&wlan_reglock, wlan_reglock_flags);
-		/* need delay between v_bat & reg_on for 2 cycle @ 38.4MHz */
-		udelay(5);
+		s3c_gpio_cfgpin(GPIO_WLAN_BT_EN, S3C_GPIO_OUTPUT);
+		s3c_gpio_setpull(GPIO_WLAN_BT_EN, S3C_GPIO_PULL_NONE);
+		gpio_set_value(GPIO_WLAN_BT_EN, GPIO_LEVEL_HIGH);
+		s3c_gpio_slp_cfgpin(GPIO_WLAN_BT_EN, S3C_GPIO_SLP_OUT1);
+		s3c_gpio_slp_setpull_updown(GPIO_WLAN_BT_EN,
+					S3C_GPIO_PULL_NONE);
+
+		msleep(80);
+	} else {
+		gpio_set_value(GPIO_WLAN_nRST, GPIO_LEVEL_LOW);
+		s3c_gpio_slp_cfgpin(GPIO_WLAN_nRST, S3C_GPIO_SLP_OUT0);
+		s3c_gpio_slp_setpull_updown(GPIO_WLAN_nRST, S3C_GPIO_PULL_NONE);
 
 		if (gpio_get_value(GPIO_BT_nRST) == 0) {
 			gpio_set_value(GPIO_WLAN_BT_EN, GPIO_LEVEL_LOW);
 			s3c_gpio_slp_cfgpin(GPIO_WLAN_BT_EN, S3C_GPIO_SLP_OUT0);
+			s3c_gpio_slp_setpull_updown(GPIO_WLAN_BT_EN,
+						S3C_GPIO_PULL_NONE);
 		}
-
-		gpio_set_value(GPIO_WLAN_nRST, GPIO_LEVEL_LOW);
-		s3c_gpio_slp_cfgpin(GPIO_WLAN_nRST, S3C_GPIO_SLP_OUT0);
-
-		spin_unlock_irqrestore(&wlan_reglock, wlan_reglock_flags);
-
-		s3c_config_gpio_alive_table(ARRAY_SIZE(wlan_sdio_off_table), wlan_sdio_off_table);
 	}
 	return 0;
 }
