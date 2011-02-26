cc0d3e1f8a00c0a762a3f12f416b107109803b5e
atinm <atin_malaviya@yahoo.com>
2011-02-26 10:15:00 -0800

    Kanged.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
-START=$(date +%m)
+START=$(date +%s)
 
-DEVICE=$1
+DEVICE="$1"
 
 case "$DEVICE" in
 	clean)
@@ -10,52 +10,40 @@ case "$DEVICE" in
 		exit
 		;;
 	captivate)
-		echo -e "\n";
-		echo "Building Kernel for Samsung Captivate"
-		echo -e "\n";
-		make aries_captivate_defconfig
+		cfg=aries_captivate_defconfig
 		;;
 	galaxys)
-		echo -e "\n";
-		echo "Building Kernel for Samsung Galaxy S"
-		echo -e "\n";
-		make aries_galaxys_defconfig
+		cfg=aries_galaxys_defconfig
 		;;
 	galaxysb)
-		echo -e "\n";
-		echo "Building Kernel for Samsung Galaxy S (b)"
-		echo -e "\n";
-		make aries_galaxys_gti9000b_defconfig
+		cfg=aries_galaxys_gti9000b_defconfig
 		DEVICE=galaxys
 		;;
 	vibrant)
-		echo -e "\n";
-		echo "Building Kernel for Samsung Vibrant"
-		echo -e "\n";
-		make aries_vibrant_defconfig
+		cfg=aries_vibrant_defconfig
 		;;
 	*)
-		echo -e "\n";
-		echo "Usage: ./build.sh DEVICE"
+		echo "Usage: $0 DEVICE"
 		echo "Example: ./build.sh galaxys"
 		echo "Supported Devices: captivate, galaxys, galaxysb, vibrant"
-		echo -e "\n";
-		exit
+		exit 2
 		;;
 esac
 
 export KBUILD_BUILD_VERSION="1"
-make -j`grep 'processor' /proc/cpuinfo | wc -l`
+echo "Using config ${cfg}"
 
-echo -e "\n";
-echo "Copying Kernel and Modules to device tree..."
+make ${cfg}  || { echo "Failed to make config"; exit 1; }
+make -j $(grep 'processor' /proc/cpuinfo | wc -l) || { echo "Failed to make kernel"; exit 1; }
+
+echo -n "Copying Kernel and Modules to device tree..."
+{
 cp arch/arm/boot/zImage ../../../device/samsung/$DEVICE/zImage
 cp drivers/net/tun.ko ../../../device/samsung/$DEVICE/tun.ko
 cp drivers/net/wireless/bcm4329/bcm4329.ko ../../../device/samsung/$DEVICE/bcm4329.ko
 cp fs/cifs/cifs.ko ../../../device/samsung/$DEVICE/cifs.ko
-echo "Done."
+} || { echo "failed to copy kernel and modules"; exit 1; }
+echo "done."
 
-echo -e "\n";
-END=$(date +%m)
-echo "Elapsed: $((END - START)) minutes."
-echo -e "\n";
+END=$(date +%s)
+echo "Elapsed: $((END - START)) sec(s)."
