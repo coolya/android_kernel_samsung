5db270507ca4228cd7214f5b182d83c500d4ba70
atinm <atinm.dev@gmail.com>
2011-05-18 00:58:37 -0400

    renamed to mtd
    Change-Id: I008b505c01156fe164657970850eef3a5085978a

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ fi
 
 targets=("$@")
 if [ 0 = "${#targets[@]}" ] ; then
-    targets=(captivatemtd galaxys galaxysb vibrant)
+    targets=(captivatemtd galaxys galaxysb vibrantmtd)
 fi
 
 START=$(date +%s)
