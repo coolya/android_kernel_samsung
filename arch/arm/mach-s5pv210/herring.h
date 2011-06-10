bb08191824382a593cd1acc3c6d4770d54455246
jt1134 <jt1134@gmail.com>
2011-06-10 18:30:26 -0500

    fascinate: fix in-call audio

--- arch/arm/mach-s5pv210/herring.h
+++ arch/arm/mach-s5pv210/herring.h
@@ -27,6 +27,12 @@ extern void s3c_setup_uart_cfg_gpio(unsigned char port);
 # define herring_is_tft_dev() (0)
 #endif
 
+#ifdef CONFIG_PHONE_ARIES_CDMA
+# define phone_is_aries_cdma() (1)
+#else
+# define phone_is_aries_cdma() (0)
+#endif
+
 extern struct s5p_panel_data herring_panel_data;
 extern struct s5p_tft_panel_data herring_sony_panel_data;
 extern struct s5p_tft_panel_data herring_hydis_panel_data;
